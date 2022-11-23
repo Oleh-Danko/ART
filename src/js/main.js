93bd91c6e58c783c1ccd2a9ce77fecb24a2ad6ac
Oleh <danko.oleh@gmail.com>
2022-11-23 13:52:53 +0200

    mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -7,4 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', '', 3000, '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical', 3000);
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
